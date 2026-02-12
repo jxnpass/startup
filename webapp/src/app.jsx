130b89881189e9c3704c073d22ddb5ebb5868139
Jackson Passey <jxnpass@gmail.com>
2026-02-12 11:24:59 -0700

    incorporate nav routes

--- webapp/src/app.jsx
+++ webapp/src/app.jsx
@@ -1,51 +1,59 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { About } from './about/about';
+import { Create } from './create/create';
+import { List } from './list/list';
+import { Bracket } from './bracket/bracket';
+import { Nav } from 'react-bootstrap';
+
 
 export default function App() {
   return (
-    <div className="body bg-dark text-light">
-      <header className="container-fluid">
-        <nav className="navbar fixed-top navbar-dark">
-          <div className="navbar-brand">
-            Bracket Builder Free<sup>&reg;</sup>
-          </div>
-          <menu className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="index.html">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="create.html">
-                Create a Bracket
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="list.html">
-                See Existing Brackets
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="about.html">
-                About
-              </a>
-            </li>
-          </menu>
-        </nav>
-      </header>
+    <BrowserRouter>
+        <div className="body bg-dark text-light">
+        <header className="container-fluid">
+            <nav className="navbar fixed-top navbar-dark">
+            <div className="navbar-brand">
+                Bracket Builder Free<sup>&reg;</sup>
+            </div>
+            <menu className="navbar-nav">
+                <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                    Home
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="/create">
+                    Create a Bracket
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="/list">
+                    See Existing Brackets
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="/about">
+                    About
+                </NavLink>
+                </li>
+            </menu>
+            </nav>
+        </header>
 
-      <main>App components go here</main>
-
-      <footer className="bg-dark text-white-50">
-        <div className="container-fluid">
-          <span className="text-reset">Jackson Passey</span>
-          <a className="text-reset" href="https://github.com/jxnpass/startup">
-            Source
-          </a>
+        <footer className="bg-dark text-white-50">
+            <div className="container-fluid">
+            <span className="text-reset">Jackson Passey</span>
+            <a className="text-reset" href="https://github.com/jxnpass/startup">
+                Source
+            </a>
+            </div>
+        </footer>
         </div>
-      </footer>
-    </div>
+    </BrowserRouter>
   );
 }
 
