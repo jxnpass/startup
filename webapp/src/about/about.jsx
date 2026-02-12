e6bc74005aa0096f8a56b0cd12e3b1d224a18bd6
Jackson Passey <jxnpass@gmail.com>
2026-02-12 11:29:23 -0700

    import about content into react

--- webapp/src/about/about.jsx
+++ webapp/src/about/about.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import './about.css';
 
 export function About() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>About displayed here</div>
+    <main>
+
+      <p>
+        This website was created by Jackson Passey as a free tool to help users create and manage tournament brackets
+        for various sports and competitions. Whether you're organizing a local sports event, a gaming tournament,
+        or any other competitive activity, Bracket Builder Free<sup>&reg;</sup> aims to simplify the process of bracket creation
+        and management.
+      </p>
+
+      <div class="row">
+        <div class="col-md-6">
+          <div id="picture" class="picture-box"><img width="200px" alt="random"
+           src="https://static.vecteezy.com/system/resources/previews/049/563/074/non_2x/icon-of-person-playing-volleyball-symbol-illustration-vector.jpg"/>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div id="quote">
+        <div><b>I like volleyball</b></div>
+        <div>- Jackson </div>
+          </div>
+        </div>
+      </div>
+      
     </main>
   );
 }
